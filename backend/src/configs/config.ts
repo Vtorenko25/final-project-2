4025eb8a47b4756bd7d72656472eda6ef3e9dc71
Vtorenko25 <oleg.vtorenko@gmail.com>
2025-08-19 20:38:38 +0300

    commit

--- backend/src/configs/config.ts
+++ backend/src/configs/config.ts
@@ -5,13 +5,8 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 3001,
 
-  // mongoUrl: process.env.MONGODB_URI || "mongodb://localhost:27017/yourdb",
   mongoUrl:
     "mongodb+srv://olegvtorenko:uLQv5KYVoWHCfwSM@cluster0.ws6iz.mongodb.net/users",
-  passwordUrl:
-    "mongodb+srv://olegvtorenko:uLQv5KYVoWHCfwSM@cluster0.ws6iz.mongodb.net/password",
-  adminEmail: process.env.ADMIN_EMAIL,
-  adminPassword: process.env.ADMIN_PASSWORD,
 
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
   jwtAccessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
