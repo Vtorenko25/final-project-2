9c0623b73f14612edf35226e9879a61c8fc8bff3
Vtorenko25 <oleg.vtorenko@gmail.com>
2026-04-13 17:48:33 +0300

    commit

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,20 +1,3 @@
-// import { Router } from "express";
-//
-// import { authController } from "../controllers/auth.controler";
-// import { authManagerMiddleware } from "../middlewares/manager.middleware";
-//
-// const router = Router();
-//
-// router.post("/sign-in", authController.signIn);
-//
-// router.post(
-//   "/sign-in/manager",
-//   authController.signInManager,
-//   authManagerMiddleware.checkManagerAccessToken,
-// );
-//
-// export const authRouter = router;
-
 import { Router } from "express";
 
 import { authController } from "../controllers/auth.controler";
